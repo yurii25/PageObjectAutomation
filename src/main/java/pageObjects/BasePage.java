65ead4dd33e65cb860a4fa32d9a8e7d289ca589c
yurii25 <baitsaryura@gmail.com>
2023-01-21 21:40:48 +0200

    init

--- src/main/java/pageObjects/BasePage.java
+++ src/main/java/pageObjects/BasePage.java
@@ -3,6 +3,11 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
@@ -13,7 +18,9 @@ public class BasePage {
         this.driver = driver;
     }
 
-
+    public void goToHomePage(){
+        driver.get("https://shop.demoqa.com/");
+    }
 
     public WebElement getWebElement(By locator){
         return driver.findElement(locator);
@@ -23,17 +30,28 @@ public class BasePage {
         getWebElement(locator).click();
     }
 
-
-
-
-
-    public WebDriver getDriver() {
-        return driver;
+    public void waitTextToBePresentInElement(By locator, String text){
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        webDriverWait.until(ExpectedConditions.textToBePresentInElementLocated(locator,
+                text));
     }
 
-    public void setDriver(WebDriver driver) {
-        this.driver = driver;
+    public boolean isElementDisplayed(By locator){
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return getWebElement(locator).isDisplayed();
     }
+
+    public void moveMouseOverTheElement(By locator){
+        Actions actions = new Actions(driver);
+        WebElement target = driver.findElement(locator);
+        actions.moveToElement(target).perform();
+    }
+
+    public String getText(By locator){
+        return driver.findElement(locator).getText();
+    }
+
 
 
 }
--- src/test/java/base/FavoriteProductAndVerifyIfFavored.java
+++ src/test/java/base/FavoriteProductAndVerifyIfFavored.java
@@ -2,17 +2,22 @@ package base;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pageObjects.FavoriteAndVerifyPageObjects;
+import pageObjects.BasePage;
 
 import java.time.Duration;
 
-public class FavoriteProductAndVerifyIfFavored extends BaseTest {
+public class FavoriteProductAndVerifyIfFavored extends BasePage {
+
+    public FavoriteProductAndVerifyIfFavored(WebDriver driver) {
+        super(driver);
+    }
 
     @Test
     public void FavoriteProductAndReviewIfFavoredTest(){
@@ -47,10 +52,47 @@ public class FavoriteProductAndVerifyIfFavored extends BaseTest {
                 .getText();
 
 
-
         System.out.println("Actual result: " + actualProductName);
         System.out.println("Expected result: " + expectedProductName);
 
         Assert.assertEquals(actualProductName, expectedProductName);
     }
+
+
+//    @Test
+//    public void FavoriteProductAndReviewIfFavoredTest100(){
+//
+//        goToHomePage();
+//
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("window.scrollBy(0,1700)");
+//
+//
+//        moveMouseOverTheElement(By.cssSelector("[href*='tokyo-talkies'] [class='second-img']"));
+//
+//
+//        isElementDisplayed(By.cssSelector("a[data-product-id='704']"));
+//        clickByLocator(By.cssSelector("a[data-product-id='704']"));
+//
+//
+//
+//        isElementDisplayed(By.cssSelector("[id='yith-wcwl-message']"));
+//
+//
+//
+//        clickByLocator(By.cssSelector("[href='https://shop.demoqa.com/wishlist/']"));
+//
+//
+//        js.executeScript("window.scrollBy(0,500)");
+//
+//
+//        String expectedProductName = "TOKYO TALKIES";
+//        String actualProductName = getText(By.xpath("//a[normalize-space()='Tokyo Talkies']"));
+//
+//
+//        System.out.println("Actual result: " + actualProductName);
+//        System.out.println("Expected result: " + expectedProductName);
+//
+//        Assert.assertEquals(actualProductName, expectedProductName);
+//    }
 }
